2e1928cb10fc721c32988b8b7bd0363f4000dc9c
Rodri <enrod_mont@outlook.es>
2023-06-13 11:47:12 -0500

    Se suben cambios

---
+++ src/app/pages/registro-de-usuarios/registro-de-usuarios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-de-usuarios',
+  templateUrl: './registro-de-usuarios.component.html',
+  styleUrls: ['./registro-de-usuarios.component.scss']
+})
+export class RegistroDeUsuariosComponent {
+
+}
